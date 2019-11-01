510124a67e73023236df30906a8dfec262488925
David Futcher <david@futcher.io>
2019-11-01 11:27:06 +0000

    Add new profile command, serialise profile changes/additions back to file

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,12 @@ extern crate serde_derive;
 extern crate ramhorns;
 
 use std::collections::HashMap;
-use std::fs::File;
+use std::error::Error;
+use std::fs;
 use std::io::prelude::*;
 use std::path::{Path, PathBuf};
 use std::process::Command;
+use std::str::from_utf8;
 use std::vec::Vec;
 
 use clap::{App, Arg, SubCommand};
@@ -15,13 +17,13 @@ use ramhorns::{Template, Content};
 use serde_derive::{Deserialize, Serialize};
 use toml::Value;
 
-#[derive(Deserialize, Serialize, Debug)]
+#[derive(Deserialize, Serialize, Debug, PartialEq)]
 struct Profile {
     #[serde(skip)]
     name: String,
     author: String,
-    username: String,
     email: String,
+    username: Option<String>,
     url: Option<String>,
 }
 
@@ -33,10 +35,52 @@ struct UrlRenderData {
 
 impl Profile {
 
+    fn new(profile_name: &str, author_name: &str, author_email: &str) -> Profile {
+        Profile{
+            name: profile_name.to_owned(),
+            author: author_name.to_owned(),
+            email: author_email.to_owned(),
+            username: None,
+            url: None,
+        }
+    }
+
+    fn with_remote_url(&mut self, remote: &str) -> &mut Profile {
+        self.url = Some(remote.to_owned());
+        self
+    }
+
+    fn with_username(&mut self, user: &str) -> &mut Profile {
+        self.username = Some(user.to_owned());
+        self
+    }
+
+    fn as_map(&self) -> HashMap<String, String> {
+        let mut map = HashMap::new();
+
+        map.insert("author".to_owned(), self.author.clone());
+        map.insert("email".to_owned(), self.email.clone());
+
+        if let Some(username) = &self.username {
+            map.insert("username".to_owned(), username.clone());
+        }
+
+        if let Some(url) = &self.url {
+            map.insert("url".to_owned(), url.clone());
+        }
+
+        map
+    }
+
     fn render_data(&self, project: String) -> UrlRenderData {
+        let username = match &self.username {
+            Some(user) => user.to_owned(),
+            _ => String::new()
+        };
+
         UrlRenderData{
             project: project,
-            username: self.username.clone()
+            username: username
         }
     }
 
@@ -66,6 +110,21 @@ impl GitProfilesApp<'_> {
                 .subcommand(
                     SubCommand::with_name("new")
                         .about("Create new profile")
+                        .arg(Arg::with_name("PROFILE")
+                                .help("Name of profile to create")
+                                .required(true))
+                        .arg(Arg::with_name("AUTHOR")
+                                .required(true))
+                        .arg(Arg::with_name("EMAIL")
+                                .required(true))
+                        .arg(Arg::with_name("USERNAME")
+                                .short("u")
+                                .long("username")
+                                .takes_value(true))
+                        .arg(Arg::with_name("URL")
+                                .short("r")
+                                .long("remote")
+                                .takes_value(true))
                 )
                 .subcommand(
                     SubCommand::with_name("list")
@@ -130,11 +189,11 @@ impl GitProfilesApp<'_> {
     }
 
     fn load_profiles(&mut self) -> Result<Vec<Profile>, std::io::Error> {
-        let path_buf = self.profiles_file_path().unwrap_or(PathBuf::from(".git_profile"));
+        let path_buf = self.profiles_file_path().unwrap_or(PathBuf::from(".git_profiles"));
         let path = path_buf.as_path();
        
         if Path::exists(path) {
-            let mut file = File::open(path)?;
+            let mut file = fs::File::open(path)?;
             let mut contents = String::new();
 
             file.read_to_string(&mut contents)?;
@@ -142,6 +201,28 @@ impl GitProfilesApp<'_> {
         }
 
         Ok(vec![])
+    }
+
+    fn save_profiles(&self, profiles: Vec<&Profile>) -> Result<(), Box<dyn Error>> {
+        // TODO: ".git_profile" magic string, this logic is used twice
+        let path_buf = self.profiles_file_path().unwrap_or(PathBuf::from(".git_profiles"));
+        let path = path_buf.as_path();
+
+        if !Path::exists(path) {
+            fs::File::create(path)?;
+        }
+
+        let mut tables: HashMap<String, toml::Value> = HashMap::new();
+        for profile in profiles {
+            let key = profile.name.clone();
+            let table = toml::Value::try_from(profile.as_map())?;
+            tables.insert(key, table);
+        }
+
+        let data = toml::to_string_pretty(&tables)?;
+        fs::write(path, data.as_bytes())?;
+
+        Ok(())
     }
 
     fn get_profile(&self, profile_name: String) -> Option<&Profile> {
@@ -160,11 +241,19 @@ impl GitProfilesApp<'_> {
         None
     }
 
-    fn get_default_profile(&self) -> Option<&Profile> {
-        // TODO: Need to handle the case this is run in a non-gitified dir
+    fn get_profile_in_local_use(&self) -> Option<&Profile> {
+        // TODO: Need to handle the case this is run in a non-git dir. Manually detect ./.git dir?
         let email = git_command(vec!["config", "user.email"]);
         if let Some(profile) = self.get_profile_by_email(email) {
             return Some(profile);
+        }
+
+        None
+    }
+
+    fn get_default_profile(&self) -> Option<&Profile> {
+        if let Some(profile) = self.get_profile_in_local_use() {
+            return Some(profile)
         }
 
         if let Some(profiles) = &self.profiles {
@@ -176,13 +265,13 @@ impl GitProfilesApp<'_> {
         None
     }
 
-    /// Unwraps the profile name (or falls back to a reasonable default) then executes the closure with the profile
-    /// as it's argument
-    fn with_profile<F>(&self, name: Option<String>, f: F) 
+    /// Unwraps the profile name, finds a matching profile (or falls back to a reasonable default) then executes the 
+    /// closure with the profile as it's argument.
+    fn with_profile<F>(&self, name: Option<&str>, f: F) 
         where F: Fn(&Profile) -> ()
     {
         let profile_opt = match name {
-            Some(name) => self.get_profile(name),
+            Some(name) => self.get_profile(name.to_owned()),
             None => self.get_default_profile()
         };
 
@@ -205,8 +294,19 @@ impl GitProfilesApp<'_> {
                 return
             }
 
+            let local_profile = self.get_profile_in_local_use();
+
             for profile in profiles {
-                println!("{}", profile.name);
+
+                print!("{}", profile.name);
+
+                if let Some(local) = local_profile {
+                    if local == profile {
+                        print!(" *");
+                    }
+                }
+
+                print!("\n");  // TODO: Does this work cross-platform?
             }
         } else {
             no_profiles();
@@ -214,15 +314,17 @@ impl GitProfilesApp<'_> {
     }
 
     fn handle_use(&self, target: String) {
-        let profile = self.get_profile(target)
-            .expect("Could not find target profile"); // TODO: handle error case
+        // We never want to fallback to a default when dealing with 'use' cmd, so we don't use `with_profile`, instead
+        // handle profile lookup manually
+        let profile = self.get_profile(target).expect("Could not find target profile");
 
         // TODO: These have results
+        // TODO: handle --global option here?
         git_command(vec!["config", "user.name", profile.author.as_ref()]);
         git_command(vec!["config", "user.email", profile.email.as_ref()]);
     }
 
-    fn handle_url(&self, project_name: String, profile_name: Option<String>) {
+    fn handle_url(&self, profile_name: Option<&str>, project_name: String) {
         self.with_profile(profile_name, |p| {
             let urlspec = match &p.url {
                 Some(url) => url.as_ref(),
@@ -230,12 +332,36 @@ impl GitProfilesApp<'_> {
             };
 
             let template = Template::new(urlspec).expect("Failed to create template from urlspec");
-            println!("{}", template.render(&p.render_data(project_name.to_string())));
+            println!("{}", template.render(&p.render_data(project_name.to_owned())));
         });
     }
 
-    fn handle_author(&self, profile_name: Option<String>) {
+    fn handle_author(&self, profile_name: Option<&str>) {
         self.with_profile(profile_name, |p| println!("{} <{}>", p.author, p.email));
+    }
+
+    fn handle_new(&self, profile_name: &str, author_name: &str, author_email: &str, username: Option<&str>, 
+                    remote: Option<&str>) 
+    {
+        let mut profile = Profile::new(profile_name, author_name, author_email);
+
+        if let Some(user) = username {
+            profile.with_username(user);
+        }
+
+        if let Some(url) = remote {
+            profile.with_remote_url(url);
+        }
+
+        let mut new_profiles = Vec::new();
+        new_profiles.extend(self.profiles.as_ref().unwrap());
+        new_profiles.push(&profile);
+
+        let result = self.save_profiles(new_profiles);
+        match result {
+            Ok(_) => println!("Profile {} created", profile_name),
+            Err(e) => println!("Profile create failed: {}", e)
+        };
     }
 }
 
@@ -247,9 +373,9 @@ fn git_command(args: Vec<&str>) -> String {
     }
 
     let output_streams = command.output().expect("failed to execute process");
-    let output = std::str::from_utf8(&output_streams.stdout).unwrap().trim_end();
+    let output = from_utf8(&output_streams.stdout).unwrap().trim_end();
 
-    return output.to_string();
+    return output.to_owned();
 }
 
 fn main() {
@@ -258,19 +384,27 @@ fn main() {
     if let Some(args) = &app.args {
         match args.subcommand() {
             ("list", _) => app.handle_list(),
-            ("new", _) => println!("New!"),
+            ("new", Some(sub_matches)) => {
+                let profile_name = sub_matches.value_of("PROFILE").expect("failed to parse profile name");
+                let author_name = sub_matches.value_of("AUTHOR").expect("failed to parse author name");
+                let author_email = sub_matches.value_of("EMAIL").expect("failed to parse author email");
+                let url = sub_matches.value_of("URL");
+                let username = sub_matches.value_of("USER");
+
+                app.handle_new(profile_name, author_name, author_email, username, url);
+            },
             ("use", Some(sub_matches)) => {
-                let profile = sub_matches.value_of("PROFILE").unwrap();
-                app.handle_use(profile.to_string());
+                let profile_name = sub_matches.value_of("PROFILE").expect("failed to parse profile name");
+                app.handle_use(profile_name.to_owned());
             },
             ("url", Some(sub_matches)) => {
-                let project_name = sub_matches.value_of("PROJECT").unwrap();
-                let profile_name = sub_matches.value_of("PROFILE").map(|x| x.to_string());
-                app.handle_url(project_name.to_string(), profile_name);
+                let project_name = sub_matches.value_of("PROJECT").expect("failed to parse project name");
+                let profile_name = sub_matches.value_of("PROFILE");
+                app.handle_url(profile_name, project_name.to_owned());
             },
             ("author", Some(sub_matches)) => {
-                let profile = sub_matches.value_of("PROFILE").map(|x| x.to_string());
-                app.handle_author(profile);
+                let profile_name = sub_matches.value_of("PROFILE");
+                app.handle_author(profile_name);
             }
             _ => println!("{}", args.usage()), // TODO: Should list sub-commands
         };
